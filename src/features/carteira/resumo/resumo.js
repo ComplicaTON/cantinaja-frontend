8f2471110ac3c11c11fd62bae25e5d11c2852032
victorlae <victoralealexandre@gmail.com>
2026-07-25 15:08:07 -0300

    refactor: integracao e resolução de conflitos

--- src/features/carteira/resumo/resumo.js
+++ src/features/carteira/resumo/resumo.js
@@ -8,8 +8,8 @@ const CARD_HTML =
 
   <div class="card bg-primary border-0 shadow-sm flex-fill">
     <div class="card-body p-3 p-md-4">
-      <h6 class="card-title text-dark text-uppercase fw-bold mb-2">Total Recarregado</h6>
-      <p class="fs-2 fw-semibold text-dark m-0" id="total-recarregado">R$ 0</p>
+      <h6 class="card-title text-light text-uppercase fw-bold mb-2">Total Recarregado</h6>
+      <p class="fs-2 fw-semibold text-light m-0" id="total-recarregado">R$ 0</p>
     </div>
   </div>
 
@@ -17,8 +17,8 @@ const CARD_HTML =
 
   <div class="card bg-primary border-0 shadow-sm flex-fill">
     <div class="card-body p-3 p-md-4">
-      <h6 class="card-title text-dark text-uppercase fw-bold mb-2">Total Gasto</h6>
-      <p class="fs-2 fw-semibold text-dark m-0" id="total-gasto">R$ 0</p>
+      <h6 class="card-title text-light text-uppercase fw-bold mb-2">Total Gasto</h6>
+      <p class="fs-2 fw-semibold text-light m-0" id="total-gasto">R$ 0</p>
     </div>
   </div>
 
@@ -26,8 +26,8 @@ const CARD_HTML =
 
   <div class="card bg-primary border-0 shadow-sm flex-fill">
     <div class="card-body p-3 p-md-4">
-      <h6 class="card-title text-dark text-uppercase fw-bold mb-2">Quantidade de Transações</h6>
-      <p class="fs-2 fw-semibold text-dark m-0" id="quantidade-transacoes">0</p>
+      <h6 class="card-title text-light text-uppercase fw-bold mb-2">Quantidade de Transações</h6>
+      <p class="fs-2 fw-semibold text-light m-0" id="quantidade-transacoes">0</p>
     </div>
   </div>
 
