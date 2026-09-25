eedf71911cbe526097c56959c9834f94b895121c
welitonfrancisco <welitonfrancisco23@gmail.com>
2026-09-24 21:21:54 -0300

    função de alterar disponibilidade adicionada

--- src/features/cardapio/alterar-disponibilidade/alterarDisponibilidade.js
+++ src/features/cardapio/alterar-disponibilidade/alterarDisponibilidade.js
@@ -1,10 +1,11 @@
 import { CONFIG_API } from "/lib/config.js";
 
-export async function alterarDisponibilidade(produto){
+export async function alterarDisponibilidade(produto, corpoDoCard, elementoBotao){
 
     try {
     const novoStatus = !produto.disponibilidade;
-    const url = `${CONFIG_API.cardapio.baseUrl}/api/items/${currentItem.id}`;
+    const url = `${CONFIG_API.cardapio.baseUrl}/api/items/${produto.id}`;
+
 
     const resposta = await fetch (url,{
         method: 'PATCH',
@@ -16,7 +17,65 @@ export async function alterarDisponibilidade(produto){
         })
     });
 
+    if (resposta.status === 409){
+        const respostaGet = await fetch(url);
+        if (!respostaGet.ok) throw new Error('Não foi possivel sincronizar.');
+
+        const produtoAtualizado = await respostaGet.json();
+        produto.disponibilidade = produtoAtualizado.disponibilidade;
+        atualizarInterface(produto, corpoDoCard, elementoBotao);
+
+        alert('Este item já foi modificado. O produto foi sincronizado');
+        return;
+    }
+
+    if (!resposta.ok){
+        throw new Error(`Erro ao alterar disponibilidade ${resposta.status}`);
+    }
+
+    produto.disponibilidade = novoStatus;
+    atualizarInterface(produto, corpoDoCard, elementoBotao);
+    
     } catch (error) {
         console.error("Erro ao alterar disponibilidade:", error);
+        throw error;
     }
-};
+}
+
+function atualizarInterface(produto, corpoDoCard, elementoBotao) {
+    if (!corpoDoCard) {
+        return;
+    }
+
+    const elementoBadge = corpoDoCard.querySelector("#badge");
+
+    if (produto.disponibilidade) {
+        
+        if (elementoBadge) {
+            elementoBadge.textContent = 'Disponível';
+            elementoBadge.classList.remove('badge-indisponivel');
+            elementoBadge.classList.add('badge-disponivel');
+        }
+
+        if (elementoBotao) {
+            elementoBotao.textContent = 'Marcar Indisponível';
+            elementoBotao.className = 'btn btn-primary mb-0 ms-auto';
+        }
+        corpoDoCard.classList.remove('card-body-disabled');
+
+
+    } else if (!produto.disponibilidade){
+
+        if (elementoBadge) {
+            elementoBadge.textContent = 'Indisponível'
+            elementoBadge.classList.remove('badge-disponivel')
+            elementoBadge.classList.add('badge-indisponivel')
+        }
+
+        if (elementoBotao) {
+            elementoBotao.textContent = 'Marcar Disponível';
+            elementoBotao.className = 'btn btn-secondary mb-0 ms-auto';
+        }
+        corpoDoCard.classList.add('card-body-disabled');
+    }
+}
--- src/features/cardapio/alterar-disponibilidade/btnDisponibilidade.js
+++ src/features/cardapio/alterar-disponibilidade/btnDisponibilidade.js
@@ -1,20 +1,20 @@
 import { alterarDisponibilidade } from "./alterarDisponibilidade";
 
-export function botaoDisponibilidade(produto){
+export function botaoDisponibilidade(produto, corpoDoCard){
 
     const btnDisponibilidade = document.createElement('button');
     
     if(produto.disponibilidade){
         btnDisponibilidade.className = 'btn btn-primary mb-0 ms-auto';
-        btnDisponibilidade.textContent = 'Marcar Indisponivel';
+        btnDisponibilidade.textContent = 'Marcar Indisponível';
 
     } else {
         btnDisponibilidade.className = 'btn btn-secondary mb-0 ms-auto';
-        btnDisponibilidade.textContent = 'Marcar Disponivel';
+        btnDisponibilidade.textContent = 'Marcar Disponível';
     }
 
     btnDisponibilidade.addEventListener("click", () => {
-        alterarDisponibilidade(produto);
+        alterarDisponibilidade(produto, corpoDoCard, btnDisponibilidade)
     })
 
     return btnDisponibilidade;
